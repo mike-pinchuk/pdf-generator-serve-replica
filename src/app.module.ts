0b01262513c4f4cf112d69b1569a70572899cd97
mike-pinchuk <morning.sunshine.work@gmail.com>
2021-01-13 10:03:57 +0200

    work on changes, added user.module file...

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {HeadlessChromeModule} from "./headless-chrome/headless-chrome.module";
-import { UserController } from './user/user.controller';
-import { UserServices } from './user/user.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [HeadlessChromeModule],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserServices],
+  imports: [HeadlessChromeModule, UserModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
