299f693c4d05ea3ab82e5f8541ab0740b5064770
mike-pinchuk <morning.sunshine.work@gmail.com>
2021-01-13 17:56:49 +0200

    work on database connection using typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Dependencies, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HeadlessChromeModule } from "./headless-chrome/headless-chrome.module";
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { typedEnv } from './utils/typed-env';
+import { Connection } from 'typeorm';
+import { UserEntity } from './user/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -14,10 +16,12 @@ import { typedEnv } from './utils/typed-env';
     username: typedEnv.DB_USER,
     password: typedEnv.DB_PASSWORD,
     database: typedEnv.DB_NAME,
-    entities: [__dirname + '/**/*.entity{.ts,.js}']
+    entities: [UserEntity]
   }),
     HeadlessChromeModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private connection: Connection) { }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,34 @@
 
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 import { CreateUserDto } from "./DTO/create-user.dto";
+import { UserEntity } from "./user.entity";
 
 @Injectable()
 export class UserServices {
+    constructor(@InjectRepository(UserEntity)
+    private usersRepository: Repository<UserEntity>
+    ) { }
     private users = []
 
-    getAll() {
-        return this.users
+    getAll(): Promise<UserEntity[]> {
+        return this.usersRepository.find()
     }
 
-    getUserById(id) {
-        return this.users.find(user => user.id === id)
+    getUserById(id: string): Promise<UserEntity> {
+        return this.usersRepository.findOne(id)
     }
 
     createUser(userDto: CreateUserDto) {
-        this.users.push({
-            ...userDto,
-            id: Date.now().toString()            
-        })
-        return {
-            users: this.users
-        }
+        this.usersRepository.insert(userDto)
+        return this.getAll()
+        // this.users.push({
+        //     ...userDto,
+        //     id: Date.now().toString()
+        // })
+        // return {
+        //     users: this.users
+        // }
     }
 }
