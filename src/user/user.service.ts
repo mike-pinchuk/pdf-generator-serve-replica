6207c06c809e84132b5243054f8ff56f80c7e171
mike-pinchuk <morning.sunshine.work@gmail.com>
2021-01-14 15:58:56 +0200

    fix(user.service.ts): upgrate getUserById function

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,6 @@
 
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { string } from "joi";
 import { Repository } from "typeorm";
 import { CreateUserDto } from "./DTO/create-user.dto";
 import { UserEntity } from "./user.entity";
@@ -11,18 +10,16 @@ export class UserServices {
     constructor(@InjectRepository(UserEntity)
     private usersRepository: Repository<UserEntity>
     ) { }
-    private users = []
 
     getAll(): Promise<UserEntity[]> {
         return this.usersRepository.find()
     }
 
-    getUserById(id: string): Promise<UserEntity> {
+    getUserById(id: string): Promise<UserEntity> | undefined {
         return this.usersRepository.findOne(id)
     }
 
     createUser(userDto: CreateUserDto) {
-        this.usersRepository.save(userDto)
-        return `Email ${userDto.email} has successfully been added`
+        return this.usersRepository.save(userDto)
     }
 }
